9063a9ff51d03f1bce3514770f39cb9becac5bb3
Dang Tu <anhtuhanam1@gmail.com>
2025-02-23 00:41:18 +0700

    update\

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,7 +2,7 @@
 <html>
 	
 <head>
-	@if(isset($config) && !isset($config['error']))
+	<!-- @if(isset($config) && !isset($config['error']))
         @foreach($config as $key => $value)
             <meta property="{{ $key }}" content="{{ $value }}">
         @endforeach
@@ -11,7 +11,20 @@
 	{!! setting('integration.google_analytics') !!}
 	{!! setting('integration.facebook_pixel') !!}
 
-    @endif
+    @endif -->
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:type" content="video.other">
+    <meta property="og:title" content="Khải Azcam">
+    <meta property="og:description" content="Hôm nào thấy có đầu bếp Kim là biết được ăn ngon">
+    <meta property="og:url" content="https://www.facebook.com/khaintt/posts/h%C3%B4m-n%C3%A0o-th%E1%BA%AFy-c%C3%B3-%C4%91%E1%BA%A7u-b%E1%BA%BFp-kim-l%C3%A0-bi%E1%BA%BFt-%C4%91%C6%B0%E1%BB%A3c-%C4%83n-ngon/10227521239804067/">
+    <meta property="og:image" content="https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/480772113_10227521226883744_7594400644901902110_n.jpg?stp=cp6_dst-jpg_tt6&cstp=mx2048x1536&ctp=p600x600&_nc_cat=111&ccb=1-7&_nc_sid=b96d88&_nc_ohc=fjONr9qVIikQ7kNvgEdBT4Z&_nc_oc=AdjNtR707dzbGMK_CpPg3lS1slCtDdVMnCaAURDt2rd-z268z-TUF6O_NxytBlc4sbdT4GycLyDcFONIxkfocok4&_nc_zt=23&_nc_ht=scontent-sin6-1.xx&_nc_gid=AYg4YXMhJXMrIe6DK41kcpP&oh=00_AYBVZW9hefSrLI2OSWcJvxA1Q1-G8C3n77z_IbLVIkrp2g&oe=67BFC5E7">
+    <meta property="og:image:alt" content="Khải Azcam">
+    <meta property="og:locale" content="en_US">
+
+    <title>Khải Azcam</title>
 	
 </head>
 <body>
