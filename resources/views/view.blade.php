709965cec2bbf39361f97b2b29564efdc586baca
Dang Tu <anhtuhanam1@gmail.com>
2025-02-23 00:52:58 +0700

    update\

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -23,7 +23,8 @@
 <meta property="og:type" content="website">
 <meta property="og:title" content="SunShine Tour">
 <meta property="og:description" content="Travel Blog">
-<meta property="og:image" content="{{asset('logo04.png')}}">
+<meta property="og:image" content="https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/480772113_10227521226883744_7594400644901902110_n.jpg?stp=cp6_dst-jpg_tt6&cstp=mx2048x1536&ctp=p600x600&_nc_cat=111&ccb=1-7&_nc_sid=b96d88&_nc_ohc=fjONr9qVIikQ7kNvgEdBT4Z&_nc_oc=AdjNtR707dzbGMK_CpPg3lS1slCtDdVMnCaAURDt2rd-z268z-TUF6O_NxytBlc4sbdT4GycLyDcFONIxkfocok4&_nc_zt=23&_nc_ht=scontent-sin6-1.xx&_nc_gid=AYg4YXMhJXMrIe6DK41kcpP&oh=00_AYBVZW9hefSrLI2OSWcJvxA1Q1-G8C3n77z_IbLVIkrp2g&oe=67BFC5E7">
+<meta property="og:image:alt" content="Khải Azcam">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Sunshine Tours</title>
 </head>
