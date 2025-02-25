8dd000815b3bbde938707f41b0de08f165a3abbc
Dang Tu <anhtuhanam1@gmail.com>
2025-02-26 01:33:01 +0700

    update

--- app/Facades/Statistic.php
+++ app/Facades/Statistic.php
@@ -183,23 +183,23 @@ class Statistic {
 		// Chia dữ liệu theo từng nhóm
 		$userTable = $groupedStats->groupBy('users_id')->map(function ($items) {
 			return $items->sum('count');
-		})->sortDesc(null);
+		})->sortByDesc(null);
 
 		$referer = $groupedStats->groupBy('referer')->map(function ($items) {
 			return $items->sum('count');
-		})->sortDesc(null);
+		})->sortByDesc(null);
 
 		$device = $groupedStats->groupBy('device')->map(function ($items) {
 			return $items->sum('count');
-		})->sortDesc(null);
+		})->sortByDesc(null);
 
 		$platform = $groupedStats->groupBy('platform')->map(function ($items) {
 			return $items->sum('count');
-		})->sortDesc(null);
+		})->sortByDesc(null);
 
 		$browser = $groupedStats->groupBy('browser')->map(function ($items) {
 			return $items->sum('count');
-		})->sortDesc(null);
+		})->sortByDesc(null);
 
 		// Trả về kết quả
 		return (object) [
