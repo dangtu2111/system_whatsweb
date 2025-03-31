19c07d5cdb1aead5146febb1324fbe2107984a77
Dang Tu <anhtuhanam1@gmail.com>
2025-04-01 01:11:26 +0700

    hupdate

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -594,7 +594,7 @@ class LinkController extends Controller
         if ($randomUrl) {
             // Append ID vào excludedIds bằng array_push
             array_push($excludedIds, $randomUrl->id);
-            Cache::put($cacheKeyVisited, $excludedIds);
+            Cache::put($cacheKeyVisited, $excludedIds, now()->addMinutes(setting('features.custom_update_min')));
 			dd($cacheKeyVisited,$excludedIds,Cache::get($cacheKeyVisited));
         } elseif (!empty($excludedIds)) {
             // Nếu không còn link để chọn, reset cache và chọn lại
