ad8a990ecfbc10fc83bb5e725d86bab57eb6b34b
Dang Tu <anhtuhanam1@gmail.com>
2025-04-01 00:45:05 +0700

    hupdate

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -556,7 +556,7 @@ class LinkController extends Controller
 
         if ($randomUrl) {
             Cache::put($cacheKey1, Carbon::now()->timestamp, now()->addMinutes(setting('features.custom_update_min')));
-            dd($excludedIds);
+        
             // Append ID vào excludedIds bằng array_push
             array_push($excludedIds, $randomUrl->id);
             Cache::put($cacheKeyVisited, $excludedIds);
